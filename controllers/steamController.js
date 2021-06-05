30ef833cfa56303b7b2da3727fd524174ebbcd9f
Gioele Rossi <gioelerossi@Gioeles-Mac-mini.local>
2021-06-05 14:51:33 +0100

    added more api stuff

--- controllers/steamController.js
+++ controllers/steamController.js
@@ -7,11 +7,28 @@ const steamModel = require('../models/steamModel')
 
 
 // url ' https://api.steampowered.com/ISteamApps/GetAppList/v2/'+'v2/?key=788BB881FB9E4532A765F61E7C1D7847'
+//price https://partner.steam-api.com/ISteamUser/GetAppPriceInfo/v1/
 steam.get('/', async (req, res) => {
     try {
         const KEY = "788BB881FB9E4532A765F61E7C1D7847";
         const api_res = await fetch(
             `https://api.steampowered.com/ISteamApps/GetAppList/v2/?key=${KEY}`
+        );
+        response = await api_res.json();
+        console.log(response);
+        res.status(200).json(response);
+    } catch (error) {
+        console.log('error: ', error);
+        return res.status(400).send('error: ' + error.toString());
+    }
+
+});
+
+steam.get('/price', async (req, res) => {
+    try {
+        const KEY = "788BB881FB9E4532A765F61E7C1D7847";
+        const api_res = await fetch(
+            `https://partner.steam-api.com/ISteamUser/GetAppPriceInfo/v1/?key=${KEY}`
         );
         response = await api_res.json();
         console.log(response);
